9e7c1ffa30d6df848f3ba1bc88e715c571a8cefb
Ajay <81299123+Lucifer007-max@users.noreply.github.com>
2026-08-25 13:46:59 +0530

    Update layout.tsx

--- doc-ai-app/app/layout.tsx
+++ doc-ai-app/app/layout.tsx
@@ -26,7 +26,12 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <>
+        <h1>Kuttu Please I m sorrry I wont repeet it again please dont leave me i will make sure this mistake will not happen kuttu please this is not the solustion na kuttu to run away from relations i know i did mistake its doesnt mean running away from relation ship can be the solution please kuttu dont go na I cant be without u please kuttu last time if its happen again i swear its cost my life please kuttu please dont leave me </h1>
+        {children}
+        </>
+      </body>
     </html>
   );
 }
